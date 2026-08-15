e6519c135fb265d6472f55ee663e7cd9dceb5353
byteforger <himanshushekhar.1778@gmail.com>
2026-08-16 02:30:31 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mpp;
+        unordered_map<char,int> mpp1;
+        int n=s.size();
+        int m=t.size();
+        if(n!=m){
+            return false;
+        }
+        for(int i=0;i<n;i++){
+            mpp[s[i]]++;
+        }for(int i=0;i<m;i++){
+            mpp1[t[i]]++;
+        }
+        for(int i=0;i<n;i++){
+            if(mpp[s[i]]!=mpp1[s[i]]){
+                return false;
+            }
+
+        }
+        return true;
+
+
+        
+    }
+};
